1d470d11915c186d381852d8d3dd675448ac20d5
Krek0204 <vlad020406@yandex.ru>
2025-05-23 14:01:34 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,56 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+
+void Train::addCar(bool light_t) {
+  if (first == nullptr) {
+    first = new Car;
+    first->light = light_t;
+    first->next = nullptr;
+    first->prev = nullptr;
+  } else {
+    Car* cur = first;
+    while (cur->next != nullptr) {
+      cur = cur->next;
+    }
+    cur->next = new Car;
+    cur->next->light = light_t;
+    cur->next->prev = cur;
+    cur->next->next = first;
+    first->prev = cur;
+  }
+}
+
+int Train::getLength() { int length = 1;
+  Car* cur = first;
+  if (cur == nullptr) return 0;
+  cur->light = true;
+  while (true) {
+    cur = cur->next;
+    countOp++;
+    if (cur->light == false) {
+      cur->light = true;
+      length++;
+      continue;
+    } else {
+      cur->light = false;
+      int steps = length;
+      while (steps--) {
+        cur = cur->prev;
+        countOp++;
+      }
+      if (cur->light == false) return length;
+      else {
+        steps = length;
+        while (steps--) {
+          cur = cur->next;
+          countOp++;
+        }
+        cur->light = true;
+        length++;
+        continue;
+      }
+    }
+  }
+}
+
+int Train::getOpCount() { return countOp; }
